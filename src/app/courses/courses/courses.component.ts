cdd3c8cfa81a5b0139f212983c75676543975f9b
Handrei Fontana <handreifontana@gmail.com>
2022-07-12 10:35:05 -0300

    feat(Table): table created in courses module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Frontend' },
+    { _id: '2', name: 'React', category: 'Frontend' },
+    { _id: '3', name: 'React-Native', category: 'Mobile' },
+    { _id: '4', name: 'Flutter', category: 'Mobile' },
+  ]
+  displayedColumns = ['name', 'category']
 
   constructor() { }
 

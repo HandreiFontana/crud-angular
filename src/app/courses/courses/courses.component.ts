8811529eccf7376cddf3c1ac39d7b4c7acf3703a
Handrei Fontana <handreifontana@gmail.com>
2022-07-13 11:57:44 -0300

    feat(Table): add, edit and delete icons added

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -14,7 +14,7 @@ import { ErrorDialogComponent } from './../../shared/components/error-dialog/err
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category', 'actions']
 
   constructor(
     private coursesService: CoursesService,

1af6a961e56b674e687b1f245790339ac5cf290f
Handrei Fontana <handreifontana@gmail.com>
2022-07-13 14:44:02 -0300

    feat(form): submit form; cancel button

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,31 +1,50 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
-  form: FormGroup
-
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
-      category: [null]
-    })
-   }
-
-  ngOnInit(): void {
+      category: [null],
+    });
   }
 
-  onSubmit() {
+  ngOnInit(): void {}
 
+  onSubmit() {
+    this.service.save(this.form.value).subscribe({
+      next: (result) => this.onSuccess,
+      error: (error) => this.onError(),
+    });
+  }
+
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
+    this.location.back();
+  }
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
+    this.location.back();
   }
 
   onReturn() {
-
+    this.location.back();
   }
-
 }
